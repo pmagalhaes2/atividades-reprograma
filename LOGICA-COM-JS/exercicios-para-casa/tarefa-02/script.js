881baa3773d41e047ab415eda9fbece154f8f26e
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-05-03 10:41:06 -0300

    Inserindo mensagem de retorno ao usuário

--- LOGICA-COM-JS/exercicios-para-casa/tarefa-02/script.js
+++ LOGICA-COM-JS/exercicios-para-casa/tarefa-02/script.js
@@ -11,3 +11,6 @@ let preco = parseFloat(prompt("Informe o preço da mercadoria: "));
 let desconto = parseFloat(prompt("Informe o percentual de desconto: "))/100;
 let valorAPagar = preco - (preco * desconto);
 let valorDesconto = preco - valorAPagar;
+
+alert(`O valor a ser pago após o desconto de ${desconto * 100}% é: R$${valorAPagar.toFixed(2)}\n
+Visto que, a quantia economizada foi de: R$${valorDesconto.toFixed(2)}`);
