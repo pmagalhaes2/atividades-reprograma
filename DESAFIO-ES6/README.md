d02746b92ed2bd7892748ad54d92e03d60036ac0
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-06-24 01:18:56 -0300

    Inserindo resolução do desafio

---
+++ DESAFIO-ES6/README.md
@@ -0,0 +1,69 @@
+# On17-TodasEmTech - Semana do Desafio (19/6 e 23/6) - 2022 - Frontend
+
+
+# Desafio de API - Github Search
+
+Neste projeto, você  criará uma aplicação de pesquisa que usa a API GitHub para recuperar informações do usuário quando um nome de usuário válido é inserido. Ele deve exibir avatar, nome de usuário, contagem de seguidores, contagem de repositórios.
+
+
+## Fase 1
+- [ ] Criar o layout seguindo o Figma disponibilizado.
+- [ ] O Input recebe o username de um usuário. Quando clicar no botão de pesquisa, caso o usuário exista dentro da base de dados da API, a aplicação deve mostrar os dados desta forma: 
+   - Exibir o avatar.
+   - Nome do usuário.
+   - Username
+   - Bio
+   - Total de seguidores
+   - Total de repositórios
+
+**Exemplo**: 
+<p align="center">
+  <img src="./images/profile.png" />
+</p>
+
+- [ ] Validar campo de texto para não entrar texto vazio
+- [ ] Limpar campo de texto depois que inserir o nome de usuário
+- [ ] O usuário deve receber um alerta se o nome de usuário não for válido 
+  
+
+**Exemplo**: 
+<p align="center">
+  <img src="./images/not-found.png" />
+</p>
+
+
+
+## Fase 2 - Extra
+
+- [ ] Exibir todos repositórios do usuário, com as informações:
+    - Titulo
+    - Descrição
+    - Linguagem utilizada
+    - Total de estrelas
+- [ ] O usuário deve receber um alerta caso usuário não tenha repositórios públicos  
+
+**Exemplo**: 
+<p align="center">
+  <img src="./images/profile-repos.png" />
+</p>
+<p align="center">
+  <img src="./images/repos-nao-encontrado.png" />
+</p>
+
+## Como vou entregar o projeto?
+- Forke o projeto, depois faça uma pasta com o seu nome ex. viviane-miranda dentro da pasta entrega-projeto. Suba as alterações commitadas e quando finalizado, faça um Pull Request. 
+
+- Importante: Tudo bem se você não conseguir finalizar o desafio todo ou não realizar a parte Extra, o importante é tentar começar! Se ajudem entre si :)
+
+# Links e recursos úteis
+
+- [Layout do projeto](https://www.figma.com/file/UjuUSqwVpb7OtbWysQZffj/github-search?node-id=0%3A1)
+- [Documentação - API GitHub](https://docs.github.com/pt/rest)
+
+
+### Agradecimento
+
+* Nat Luiza <3
+
+
+
---
+++ DESAFIO-ES6/githubSearch/script.js
@@ -0,0 +1,109 @@
+const input = document.querySelector(".input");
+const button = document.querySelector(".search-button");
+const section = document.querySelector("section");
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  const userName = input.value.trim();
+  userName ? getGitHubData(userName) : alert("Please enter a valid username");
+  input.value = "";
+});
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const userName = input.value.trim();
+    userName ? getGitHubData(userName) : alert("Please enter a valid username");
+    input.value = "";
+  }
+});
+
+async function getGitHubData(user) {
+  const url = `https://api.github.com/users/${user}`;
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      createCard(data);
+    } else {
+      handleUserNotFound();
+    }
+  } catch (err) {
+    console.error("Request was not successful", err);
+  }
+}
+
+async function createCard(user) {
+  const { name, avatar_url, bio, public_repos, login, followers } = user;
+  section.innerHTML = `
+  <div class="card">
+  <img class="profile-img" src="${avatar_url}" alt="foto do avatar do github">
+  <h2 class="title-green">${name}</h2>
+  <h3>${login}</h3>
+  <br>
+  <p>${bio ? bio : ""}</p>
+  <div class="line-empty"></div>
+  <div class="profile-informations">
+  <img class="icons" src="images/followers.png">
+  <p><b>${followers}</b> followers</p>
+  <a class="repositories-link"><img class="icons" src="images/repository.png"></a>
+  <p><b>${public_repos}</b> repositories</p>
+  </div>
+  </div>
+  `;
+
+  const repositoriesLink = document.querySelector(".repositories-link");
+
+  repositoriesLink.addEventListener("click", (event) => {
+    getRepositories(login);
+  });
+}
+
+function handleUserNotFound() {
+  return (section.innerHTML = `
+  <img class="not-found-img" src="./images/not-found.png">
+`);
+}
+
+async function getRepositories(login) {
+  const url = `https://api.github.com/users/${login}/repos`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.length > 0) {
+      createRepositoriesCards(data);
+    } else {
+      handleNotFoundRepositories(login);
+    }
+  } catch (err) {
+    console.error("Request was not successful", err);
+  }
+}
+
+async function createRepositoriesCards(repositories) {
+  const { name, description, language, stargazers_count } = repositories;
+  const repositoriesList = document.createElement("div");
+
+  repositoriesList.setAttribute("class", "repositories-list");
+  section.appendChild(repositoriesList);
+
+  repositories.map((repository) => {
+    return (repositoriesList.innerHTML += `
+    <div class="repository">
+    <h2 class="title-green">${repository.name}</h2>
+    <h3 class="repository-description">${
+      repository.description ? repository.description : ""
+    }</h3>
+    <div class="informations">
+    <p>${repository.language}</p>
+    <p><img src="./images/star_border.png"> ${repository.stargazers_count}</p>
+    </div>
+    </div>
+    `);
+  });
+}
+
+function handleNotFoundRepositories(login) {
+  section.innerHTML += `<h2 class="not-found-repositories">${login} doesn’t have any public repositories yet.</h2>
+`;
+}
